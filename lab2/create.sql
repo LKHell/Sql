bb830b93287253d2dda4ef05fd5cea367a06278a
Li Kunhao <372383419@qq.com>
2016-11-15 16:51:45 +0800

    1
    1

---
+++ lab2/create.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Student
+(
+	Sno VARCHAR(20) PRIMARY KEY, /*studentid*/
+	SName VARCHAR(10),
+	Age INT CHECK (Age>21 AND AGE<25),
+	Sex CHAR(6) CHECK (Sex='Male' OR Sex='Female'),
+	Depart VARCHAR(30)  /*department*/
+	
+);
+
+CREATE TABLE Course
+(
+	Cno VARCHAR(15) PRIMARY KEY,
+	CName VARCHAR(30),
+	Credit INT  CHECK (Credit>0 AND Credit<10),
+	CBefore VARCHAR(15) /*coursebefore*/
+);
+
+CREATE TABLE SC
+(
+	Sno VARCHAR(20) ,
+	Cno VARCHAR(15) ,
+	Score Dec(5,2) CHECK(Score>0 AND Score<100),
+	PRIMARY KEY (Sno , Cno),
+	FOREIGN KEY(Sno) REFERENCES  Student(Sno),
+	FOREIGN KEY(Cno) REFERENCES  Course(Cno)
+);
