d43f29eb8a17a85b1a66ca6c46d7e179da3a6fd0
Li Kunhao <372383419@qq.com>
2017-06-01 00:47:14 +0800

    pord_in isnt finished

--- Oracle/privilege.sql
+++ Oracle/privilege.sql
@@ -4,6 +4,8 @@ GRANT SELECT ON purchase_pds TO ora00057;
 -- sks
 GRANT SELECT ON part_sks TO ora00059;
 --sk
-GRANT UPDATE,SELECT ON part  TO ora00137;
+GRANT SELECT,UPDATE ON part  TO ora00137;
 GRANT SELECT ON purchase_sk TO ora00137;
+GRANT UPDATE ON purchase_order To ora00137;
+GRANT SELECT,UPDATE ON detail To ora00137;
 --pa
